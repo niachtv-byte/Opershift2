d3bde07f6906cc656a33908aff1e07600b830362
niachtv-byte <niachtv@gmail.com>
2026-09-15 21:53:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ total_pendapatan_netto = total_tunai_netto + total_non_tunai_netto
 st.markdown("---")
 st.subheader("⏳ Transaksi / Tagihan Dalam Proses (Pending / Outstanding)")
 initial_pending_data = pd.DataFrame([
-    {"No. Transaksi / RM": "TRX-00129", "Keterangan / Kendala": "Menunggu konfirmasi settlement EDC", "Nominal (Rp)": 250000.0}
+    {"Nama / No RM": " ", "Keterangan / Kendala": " ", "Status / Tindak Lanjut":  }
 ])
 edited_pending_df = st.data_editor(
     initial_pending_data,
